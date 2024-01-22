c7d25f56c01433a3b79cfd6a0b20ee0eb240cd22
AnandsinghKKR <jitendrapalsingh465@gmail.com>
2024-01-22 08:21:30 +0530

    fourth change

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { CREATE_TODO, DELETE_TODO, LOGIN, REGISTER, TODO_LIST } from './apiConstants.js'
+import { CREATE_TODO, DELETE_TODO, LOGIN, MARK_TODO, REGISTER, TODO_LIST } from './apiConstants.js'
 
 export const login =async (data)=>{
     return axios.post(LOGIN,data);
@@ -21,6 +21,14 @@ export const deleteTodoApi =async (data)=>{
     let token=getToken();
     console.log("Token : " ,token);
     return axios.post(DELETE_TODO,data,{
+        headers:{auth:token}
+    });
+}
+
+
+export const MarkTodoApi =async (data)=>{
+    let token=getToken();
+    return axios.post(MARK_TODO,data,{
         headers:{auth:token}
     });
 }
